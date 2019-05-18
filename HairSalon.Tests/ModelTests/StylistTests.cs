37ee1b75773738df4feb153b1fc70591f36d5f17
Mariia <mariiapopovych@gmail.com>
2019-05-17 17:08:53 -0700

    add SpecialityTests

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -56,7 +56,72 @@ public void GetAllStylists_ReturnAllStylists()
 	List<Stylist> result = Stylist.GetAll();
 	CollectionAssert.AreEqual (result, newList);
 }
+[TestMethod]
 
+public void ClearAll_ReturnEmptyList()
+{
+	Stylist newStylist = new Stylist("Roma",3);
+	newStylist.Save();
+	Stylist newStylist1 = new Stylist("Andi",3);
+	newStylist1.Save();
+	Stylist.ClearAll();
+	List<Stylist> allStylists = Stylist.GetAll();
+	List<Stylist> result = new List<Stylist> {
+	};
+	CollectionAssert.AreEqual(allStylists,result);
+
+}
+[TestMethod]
+public void Edit_ReturnEditedStylistInfo()
+{
+	Stylist newStylist = new Stylist("Sorianna");
+	newStylist.Save();
+	newStylist.Edit("Marianna");
+	Stylist result = new Stylist ("Marianna", newStylist.GetId());
+	Assert.AreEqual (newStylist,result);
+
+}
+[TestMethod]
+public void GetClients_ReturnStylistWithAllClients()
+{
+	Stylist newStylist = new Stylist("Sorianna");
+	newStylist.Save();
+	Client newClient = new Client("Lisa", new DateTime(2002,2,4));
+	Client newClient1 = new Client("Lisa", new DateTime(2002,2,19));
+	newClient.Save();
+	newClient1.Save();
+	newStylist.AddClient(newClient);
+	newStylist.AddClient(newClient1);
+
+	List<Client> allStylistsClients = new List<Client> {
+		newClient,newClient1
+	};
+	List<Client> result = newStylist.GetClients();
+	CollectionAssert.AreEqual(allStylistsClients,result);
+
+
+}
+
+[TestMethod]
+public void GetSpecialities_ReturnStylistWithAllClients()
+{
+	Stylist newStylist = new Stylist("Sorianna");
+	newStylist.Save();
+	Speciality newSpeciality = new Speciality("Hairmaster");
+	Speciality newSpeciality1 = new Speciality("Nailmaster");
+	newSpeciality.Save();
+	newSpeciality1.Save();
+	newStylist.AddSpeciality(newSpeciality);
+	newStylist.AddSpeciality(newSpeciality1);
+
+	List<Speciality> allStylistsSpecialities = new List<Speciality> {
+		newSpeciality,newSpeciality1
+	};
+	List<Speciality> result = newStylist.GetSpecialities();
+	CollectionAssert.AreEqual(allStylistsSpecialities,result);
+
+
+}
 
 
 
